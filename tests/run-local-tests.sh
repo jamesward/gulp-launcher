21d1038a4d85ca2b60c677073cf2c61034e16911
James Ward <james@jamesward.org>
2014-12-02 16:44:00 -0700

    fix test cleanup

--- tests/run-local-tests.sh
+++ tests/run-local-tests.sh
@@ -65,7 +65,7 @@ run_test "node_0.10.x_without_gulpfile" "Starting 'default'" "yes" "rm node_0.10
 run_test "node_0.10.x_without_gulpfile" "No gulpfile.js found" "no"
 
 # Node 0.10.x without a package.json and auto-add one
-run_test "node_0.10.x_without_package_json" "Starting 'default'" "yes" "node_0.10.x_without_package_json/package.json"
+run_test "node_0.10.x_without_package_json" "Starting 'default'" "yes" "rm node_0.10.x_without_package_json/package.json"
 # Node 0.10.x without a package.json but don't auto-add one
 run_test "node_0.10.x_without_package_json" "No package.json found" "no"
 

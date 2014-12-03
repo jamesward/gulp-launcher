8063eb522291052ad466d58dc826395102a4a212
James Ward <james@jamesward.org>
2014-12-03 15:51:07 -0700

    full path for pyinstaller

--- windows/build.bat
+++ windows/build.bat
@@ -2,4 +2,4 @@ cd /d %~dp0
 python -c "import urllib2; get_pip = urllib2.urlopen('https://bootstrap.pypa.io/get-pip.py').read(); file('get_pip.py', 'w').write(get_pip)"
 python get_pip.py
 C:\Python27\Scripts\pip install -r requirements.txt
-pyinstaller --onefile gulp-launcher.py
+C:\Python27\Scripts\pyinstaller --onefile gulp-launcher.py

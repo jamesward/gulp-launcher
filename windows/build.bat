c2cd1567a609ba0c7abf3a5d426a347f7c46f7fc
James Ward <james@jamesward.org>
2014-12-03 15:45:17 -0700

    move into the windows dir

--- windows/build.bat
+++ windows/build.bat
@@ -1,3 +1,4 @@
+cd /d %~dp0
 python -c "import urllib2; get_pip = urllib2.urlopen('https://bootstrap.pypa.io/get-pip.py').read(); file('get_pip.py', 'w').write(get_pip)"
 python get_pip.py
 C:\Python27\Scripts\pip install -r requirements.txt

241cc07445bfc721a75057b9e85df89dfd159a70
James Ward <james@jamesward.org>
2014-12-04 12:12:03 -0700

    add all the tests

--- tests/tests.bat
+++ tests/tests.bat
@@ -15,6 +15,86 @@ set "TEST=node_0.10.x"
 set "EXP=Starting 'help'"
 call :run_test "%TEST%" "%EXP%"
 
+:: Gulp dep isn't set and we tell the gulp launcher not to add it
+set "TEST=no_gulp_dep"
+set "EXP=No Gulp dependency was found"
+set "STDIN=no"
+call :run_test "%TEST%" "%EXP%" "%STDIN%"
+
+:: Gulp dep isn't set and we tell the gulp launcher to add it
+set "TEST=no_gulp_dep"
+set "EXP=Starting 'help'"
+set "STDIN=yes"
+set "CLEAN=git checkout package.json"
+call :run_test "%TEST%" "%EXP%" "%STDIN%" "%CLEAN%"
+
+:: Node engine isn't set and we tell the gulp launcher not to add it
+set "TEST=no_node"
+set "EXP=Exiting because the Node version could not be determined."
+set "STDIN=no"
+call :run_test "%TEST%" "%EXP%" "%STDIN%"
+
+:: Node engine isn't set and we tell the gulp launcher to add it
+set "TEST=no_node"
+set "EXP=Starting 'help'"
+set "STDIN=yes"
+set "CLEAN=git checkout package.json"
+call :run_test "%TEST%" "%EXP%" "%STDIN%" "%CLEAN%"
+
+:: Node 0.10.x without a gulpfile and auto-add one
+set "TEST=node_0.10.x_without_gulpfile"
+set "EXP=Starting 'default'"
+set "STDIN=yes"
+set "CLEAN=rm gulpfile.js"
+call :run_test "%TEST%" "%EXP%" "%STDIN%" "%CLEAN%"
+
+:: Node 0.10.x without a gulpfile but don't auto-add one
+set "TEST=node_0.10.x_without_gulpfile"
+set "EXP=No gulpfile.js found"
+set "STDIN=no"
+call :run_test "%TEST%" "%EXP%" "%STDIN%"
+
+:: Node 0.10.x without a package.json and auto-add one
+set "TEST=node_0.10.x_without_package_json"
+set "EXP=Starting 'default'"
+set "STDIN=yes"
+set "CLEAN=rm package.json"
+call :run_test "%TEST%" "%EXP%" "%STDIN%" "%CLEAN%"
+
+:: Node 0.10.x without a package.json but don't auto-add one
+set "TEST=node_0.10.x_without_package_json"
+set "EXP=No package.json found"
+set "STDIN=no"
+call :run_test "%TEST%" "%EXP%" "%STDIN%"
+
+:: Node 0.10.x with a gulpfile
+set "TEST=node_0.10.x"
+set "EXP=Starting 'help'"
+call :run_test "%TEST%" "%EXP%"
+
+:: Node ~0.10.33
+set "TEST=node_tilde0.10.33"
+set "EXP=Starting 'help'"
+call :run_test "%TEST%" "%EXP%"
+
+:: Node ^0.10.33
+set "TEST=node_carret0.10.33"
+set "EXP=Starting 'help'"
+call :run_test "%TEST%" "%EXP%"
+
+:: Node 0.10.33
+set "TEST=node_0.10.33"
+set "EXP=Starting 'help'"
+call :run_test "%TEST%" "%EXP%"
+
+:: Node 0.10.33 with a specified task
+set "TEST=node_0.10.33"
+set "EXP=Task 'asdf' is not in your gulpfile"
+set "STDIN="
+set "CLEAN="
+set "ARGS=asdf"
+call :run_test "%TEST%" "%EXP%" "%STDIN%" "%CLEAN%" "%ARGS%"
+
 
 goto End
 
@@ -22,7 +102,7 @@ goto End
 :: Runs a test
 ::
 :: Usage:
-:: call :run_test base_dir expected_output [stdin] [cleanup]
+:: call :run_test base_dir expected_output [stdin] [cleanup] [args]
 ::
 :run_test
   setlocal EnableDelayedExpansion
@@ -30,6 +110,7 @@ goto End
   set "EXPECTED=%~2"
   set "IN=%~3"
   set "CLEANUP=%~4"
+  set "ARGS=%~5"
 
   echo Running the gulp launcher in %DIR%
   echo IN: %IN%
@@ -51,9 +132,9 @@ goto End
   set FAILED=true
 
   if not defined IN (
-    set "GULP_CMD=%GULP%"
+    set "GULP_CMD=%GULP% %ARGS%"
   ) else (
-    set "GULP_CMD=%GULP% ^< %IN%"
+    set "GULP_CMD=%GULP% %ARGS% ^< %IN%"
   )
 
   echo Running: %GULP_CMD%

c9f2a03ad5b85ed4e0e6f83e914f32c99b73f7e7
James Ward <james@jamesward.org>
2014-12-06 12:00:03 -0700

    try a different way to interact with the process

--- python/gulp.py
+++ python/gulp.py
@@ -156,7 +156,10 @@ def cmdline(arglist):
     if cf.TRACE:
         print "arglist: "
         pprint.pprint(arglist)
-    subprocess.call(arglist)
+    p = subprocess.call(arglist, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    out, err = p.communicate()
+    print(out)
+    print(err)
 
 def install_gulp():
     download_node_binary()

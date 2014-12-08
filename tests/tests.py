da0782684805a736431f81115d32bac171db5fcb
James Ward <james@jamesward.org>
2014-12-08 16:44:21 -0700

    add setup ability

--- tests/tests.py
+++ tests/tests.py
@@ -2,7 +2,7 @@ import subprocess, pprint, sys, os
 
 gulpcmd = os.path.normpath("../python/dist/gulp.exe")
 
-def run_test(dir, exp, stdin, cleanup, args):
+def run_test(dir, exp, stdin, setup, cleanup, args):
     args.insert(0, gulpcmd)
 
     print "Running {0} in {1}".format(" ".join(args), dir)
@@ -12,6 +12,11 @@ def run_test(dir, exp, stdin, cleanup, args):
 
     myenv = os.environ
     myenv["GULP_LAUNCHER_TRACE"] = "1"
+
+    if setup:
+        print "Setting up: {0}".format(setup)
+        print
+        subprocess.Popen(setup, shell=True, cwd=dir).communicate()
 
     try:
         p = subprocess.Popen(args, cwd=dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, env=myenv, shell=True)
@@ -42,36 +47,36 @@ def run_test(dir, exp, stdin, cleanup, args):
         sys.exit(1)
 
 # Gulp dep isn't set and we tell the gulp launcher not to add it
-run_test("no_gulp_dep", "No Gulp dependency was found", "no", "", [])
+run_test("no_gulp_dep", "No Gulp dependency was found", "no", "rmdir node_modules /s /q", "", [])
 # Gulp dep isn't set and we tell the gulp launcher to add it
-run_test("no_gulp_dep", "Starting 'help'", "yes", "git checkout package.json", [])
+run_test("no_gulp_dep", "Starting 'help'", "yes", "cp package.json package.json-", "mv package.json- package.json", [])
 
 # Node engine isn't set and we tell the gulp launcher not to add it
-run_test("no_node", "Exiting because the Node version could not be determined.", "no", "", [])
+run_test("no_node", "Exiting because the Node version could not be determined.", "no", "", "", [])
 # Node engine isn't set and we tell the gulp launcher to add it
-run_test("no_node","Starting 'help'", "yes", "git checkout package.json", [])
+run_test("no_node","Starting 'help'", "yes", "cp package.json package.json-", "mv package.json- package.json", [])
 
 # Node 0.10.x without a gulpfile and auto-add one
-run_test("node_0.10.x_without_gulpfile", "Starting 'default'", "yes", "rm gulpfile.js", [])
+run_test("node_0.10.x_without_gulpfile", "Starting 'default'", "yes", "", "rm gulpfile.js", [])
 # Node 0.10.x without a gulpfile but don't auto-add one
-run_test("node_0.10.x_without_gulpfile", "No gulpfile.js found", "no", "", [])
+run_test("node_0.10.x_without_gulpfile", "No gulpfile.js found", "no", "", "", [])
 
 # Node 0.10.x without a package.json and auto-add one
-run_test("node_0.10.x_without_package_json", "Starting 'default'", "yes", "rm package.json", [])
+run_test("node_0.10.x_without_package_json", "Starting 'default'", "yes", "", "rm package.json", [])
 # Node 0.10.x without a package.json but don't auto-add one
-run_test("node_0.10.x_without_package_json","No package.json found", "no", "", [])
+run_test("node_0.10.x_without_package_json","No package.json found", "no", "", "", [])
 
 # Node 0.10.x with a gulpfile
-run_test("node_0.10.x", "Starting 'help'", "", "", [])
+run_test("node_0.10.x", "Starting 'help'", "", "", "", [])
 
 # Node ~0.10.33
-run_test("node_tilde0.10.33", "Starting 'help'", "", "", [])
+run_test("node_tilde0.10.33", "Starting 'help'", "", "", "", [])
 
 # Node ^0.10.33
-run_test("node_carret0.10.33", "Starting 'help'", "", "", [])
+run_test("node_carret0.10.33", "Starting 'help'", "", "", "", [])
 
 # Node 0.10.33
-run_test("node_0.10.33", "Starting 'help'", "", "", [])
+run_test("node_0.10.33", "Starting 'help'", "", "", "", [])
 
 # Node 0.10.33 with a specified task
-run_test("node_0.10.33", "Task 'asdf' is not in your gulpfile", "", "", ["asdf"])
+run_test("node_0.10.33", "Task 'asdf' is not in your gulpfile", "", "", "", ["asdf"])

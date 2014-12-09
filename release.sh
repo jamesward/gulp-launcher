cc4e0f5c13000c521d42bfea50e52caa9f29c96f
James Ward <james@jamesward.org>
2014-12-09 13:04:17 -0700

    release process fixes

--- release.sh
+++ release.sh
@@ -5,18 +5,18 @@ if [ "$APPVEYOR_REPO_TAG" == "True" ]; then
 
   echo "Shipping a release for $APPVEYOR_REPO_BRANCH"
 
-  curl -s -o github-release.zip https://github.com/aktau/github-release/releases/download/v0.5.2/windows-amd64-github-release.zip
+  curl -s -L -o github-release.zip https://github.com/aktau/github-release/releases/download/v0.5.2/windows-amd64-github-release.zip
 
   unzip github-release.zip
 
-  github-release upload \
+  bin/windows/amd64/github-release.exe upload \
     --user jamesward \
     --repo gulp-launcher \
     --tag $APPVEYOR_REPO_BRANCH \
     --name "gulp" \
     --file bash/gulp
 
-  github-release upload \
+  bin/windows/amd64/github-release.exe upload \
     --user jamesward \
     --repo gulp-launcher \
     --tag $APPVEYOR_REPO_BRANCH \
